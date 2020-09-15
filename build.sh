3530197d71420aa8885c144dcf80322edbef3ea4
Eddie Joseph <eddie.joseph@windowslive.com>
2020-09-15 21:12:55 +0200

    fixed deployment file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker-compose build
+docker-compose -f docker-compose-build.yml build
 docker tag gcr.io/qualitool/qualitool_be:latest gcr.io/qualitool/qualitool_be:$1
 docker tag gcr.io/qualitool/qualitool_fe:latest gcr.io/qualitool/qualitool_fe:$1
 docker push gcr.io/qualitool/qualitool_be
