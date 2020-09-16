52fae0f5b6eeb87391509150805fe80700095d30
Eddie Joseph <eddie.joseph@windowslive.com>
2020-09-16 19:14:24 +0200

    fixed problem occuring in base url setting.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker-compose -f docker-compose-build.yml build
-docker push gcr.io/qualitool/qualitool_be
-docker push gcr.io/qualitool/qualitool_fe
+docker push gcr.io/qualitool/qualitool_be:latest
+docker push gcr.io/qualitool/qualitool_fe:latest
 if [ $# -eq 1 ]
 	then
 	docker tag gcr.io/qualitool/qualitool_be:latest gcr.io/qualitool/qualitool_be:$1
--- frontend/src/reducers/config.js
+++ frontend/src/reducers/config.js
@@ -1,4 +1,7 @@
-const configReducer = (state = {baseurl:"https://localhost:8082/"}, action) => {
+
+
+const configReducer = (state = {baseurl:window.location.href.split(":")[0]+"://" + window.location.host.split(":")[0]+":8082/"}, action) => {
+//const configReducer = (state = {baseurl:"https://dummy:8082/"}, action) => {
     switch(action.type) {
         case "UPDATE_CONFIG":
             return {...state, ...action.config};
