60954198adf8c212257baa11043f83d5f5c21d68
Eddie Joseph <eddie.joseph@windowslive.com>
2020-09-22 12:24:58 +0200

    removed unused dependencies.

--- backend/src/main/java/ch/eddjos/qualitool/Qualitool.java
+++ backend/src/main/java/ch/eddjos/qualitool/Qualitool.java
@@ -1,18 +1,12 @@
 package ch.eddjos.qualitool;
 
-import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Profile;
-
-import java.security.Security;
-
 
 @SpringBootApplication
 public class Qualitool extends SpringBootServletInitializer {
     public static void main(String[] args){
-        Security.addProvider(new PemKeyStoreProvider());
         SpringApplication.run(Qualitool.class, args);
     }
 }
